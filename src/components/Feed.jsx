7f3511f6e65ea3a93a82e1e8150d7a9ede620997
Gabriel Silva <gabriel.silva2f3d@gmail.com>
2023-02-15 16:56:53 -0300

    Layout of the site
    Next step, the videos!!

---
+++ src/components/Feed.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+import { Box, Stack, Typography } from "@mui/material"
+import SideBar from "./SideBar"
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}> 
+      <Box sx={{ 
+        height: { sx: 'auto', md: '92vh' }, 
+        borderRight: '1px solid #e3e3e3', 
+        px: { sx: 0, md: 2 }
+      }}>
+        <SideBar />
+        <Typography 
+          className="copyright" 
+          variant="body2" 
+          sx={{ mt: 1.5, color: '#fff' }}>
+            Copyright 2023 GabrielSilva
+        </Typography>
+      </Box>
+      <Box 
+        sx={{ 
+          flex: 2,
+          height: { sx: "auto", md: "92vh" }, 
+          overflowY: "auto" 
+        }}>
+        <Typography variant="h4"
+        fontWeight="bold" mb={2} sx={{
+          color: 'white'
+        }}>
+          New <span style = {{color: '#F31503'}}>Videos</span> 
+        </Typography>
+      </Box>
+    </Stack>
+  )
+}
+
+export default Feed
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+      sx={{
+        position: "sticky",
+        top: 0,
+        background: "#000",
+        p: 2,
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
+};
+
+export default Navbar;
